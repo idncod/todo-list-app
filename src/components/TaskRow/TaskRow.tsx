ec2ac0cd5cfc9319ea63148cd4a06302a774ada1
idncod <idncod@protonmail.com>
2025-04-07 15:04:48 +0100

    feat: add scoped TaskRow styles and global scss setup with variables and base styles

--- src/components/TaskRow/TaskRow.tsx
+++ src/components/TaskRow/TaskRow.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { TaskRowProps } from "../../types/task.ts";
+import styles from './TaskRow.module.scss';
 
 const TaskRow: React.FC<TaskRowProps> = ({task, onToggle, onDelete}) => {
     return(
-        <li>
+        <li className={styles.taskRow}>
+            <div className={styles.leftSection}>
             <input
                 type="checkbox"
+                className={styles.checkbox}
                 checked={task.completed}
                 onChange={() => onToggle(task.id)}
             />
-            <span>{task.text}</span>
-            <button onClick={() => onDelete(task.id)}>Delete</button>
+            <span className={`${styles.text} ${
+                task.completed ? styles.completed : ''
+            }`}
+            >
+                {task.text}
+            </span>
+            </div>
+            <button onClick={() => onDelete(task.id)} className={styles.deleteButton}>Delete</button>
         </li>
-
-
     )
-
-
 }
 
 export default TaskRow;
