edd1843b93544a9f761f56485b884766657b4e13
idncod <idncod@protonmail.com>
2025-04-07 15:08:55 +0100

    feat: add scss module styles to TaskList component

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import TaskRow from "../TaskRow/TaskRow.tsx";
 import {useTasks} from "../../hooks/useTasks.ts";
+import styles from './TaskList.module.scss';
 
 const TaskList: React.FC = () => {
     const { tasks, input, setInput, addTask, toggleTask, deleteTask } = useTasks();
 
     return (
-        <div>
-            <div>
+        <div className={styles.wrapper}>
+            <div className={styles.inputGroup}>
                 <input type="text"
                        value={input}
+                       className={styles.input}
                        placeholder="Add Task"
                        onChange={(e) => setInput(e.target.value)}
                        onKeyDown={(e) => e.key === 'Enter' && addTask()}
                 />
-                <button onClick={addTask}>Add Task</button>
+                <button onClick={addTask} className={styles.button}>Add Task</button>
             </div>
 
-            <ul>
+            <ul className={styles.taskList}>
                 {tasks.map((task) => (
                     <TaskRow
                         key={task.id}
